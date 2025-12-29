3a0e94c19422e90e7296e71646e8df9dc5dea8a3
XenderMd <denis.stavilamd@gmail.com>
2025-12-29 13:08:14 +0200

    creating and saving user data

--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.userService.create(body.email, body.password);
   }
 }
