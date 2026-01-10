d5f8c8e07c6bd04108704eec000ee6da3993e1ad
XenderMd <denis.stavilamd@gmail.com>
2026-01-10 22:40:45 +0200

    starting the implementation of the signup functionality

--- car-value/src/users/auth.service.ts
+++ car-value/src/users/auth.service.ts
@@ -1,7 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // See if email is in use
+    const users = await this.userService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email already in use');
+    }
+    // hash user passwords
+    // create new user and save it
+    // return the user
+  }
+
+  signin() {}
 }
