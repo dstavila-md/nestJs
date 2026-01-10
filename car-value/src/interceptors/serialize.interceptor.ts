7013fe487524b70879c127a36cc563ebfdbc3836
XenderMd <denis.stavilamd@gmail.com>
2026-01-10 17:43:05 +0200

    A bit of type safety around serialize

--- car-value/src/interceptors/serialize.interceptor.ts
+++ car-value/src/interceptors/serialize.interceptor.ts
@@ -6,23 +6,23 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToInstance } from 'class-transformer';
+import { plainToInstance, ClassConstructor } from 'class-transformer';
 
-export function Serialize(dto: any) {
+export function Serialize<T>(dto: ClassConstructor<T>) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
-export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private dto: ClassConstructor<T>) {}
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
-  ): Observable<any> | Promise<Observable<any>> {
+  ): Observable<T> | Promise<Observable<T>> {
     // Run something before a request is handled
     // by the request handler
 
     return next.handle().pipe(
-      map((data: any) => {
+      map((data: T) => {
         // Run something before the response is sent out
         return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
