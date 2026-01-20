8c66142b61821a0f5ac1243749e3964832c9c6f8
XenderMd <denis.stavilamd@gmail.com>
2026-01-21 00:01:52 +0200

    (Re)Applying globally scoped middleware to ensure compatibility with e2e tests + first custom e2e test

--- car-value/src/main.ts
+++ car-value/src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({ keys: ['random string'] }));
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
