6adf6f1c7d9f98964750c54af59593132894316e
XenderMd <denis.stavilamd@gmail.com>
2026-01-25 17:08:02 +0200

    Added a test - signup as a new user then get the currently logged in user. Updated the tests with an `afterEach` statement to close the app instance after each tests and lift the lock on the database (otherwise the db would fail to get removed between tests)

--- car-value/test/auth.e2e-spec.ts
+++ car-value/test/auth.e2e-spec.ts
@@ -15,6 +15,10 @@ describe('Authentication system', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    app.close();
+  });
+
   it('handles a signup request', () => {
     const testEmail = 'unique@test.com';
     return request(app.getHttpServer())
@@ -26,5 +30,22 @@ describe('Authentication system', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(testEmail);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const testEmail = 'unique@test.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: testEmail, password: 'uniqueTestPassword' })
+      .expect(201);
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(testEmail);
   });
 });
