695e775a3df40c1526c780ec6607e2382886c677
XenderMd <denis.stavilamd@gmail.com>
2026-01-31 19:07:31 +0200

    The many to one and one to many decorators

--- car-value/src/users/user.entity.ts
+++ car-value/src/users/user.entity.ts
@@ -5,17 +5,24 @@ import {
   AfterInsert,
   AfterRemove,
   AfterUpdate,
+  OneToMany,
 } from 'typeorm';
+
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
